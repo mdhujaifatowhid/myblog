9f869009a456886dec476d5dbc5e4b3656c75213
Md Hujaifa Towhid <mdhujaifa411@gmail.com>
2026-06-20 20:10:47 +0600

    Refactor robots.txt.js to use getSiteUrl function

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,9 +1,11 @@
+import { getSiteUrl } from '../lib/site';
+
 export default function Robots() {
   return null;
 }
 
 export async function getServerSideProps({ res }) {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || '';
+  const siteUrl = getSiteUrl();
   const body = `User-agent: *
 Allow: /
 Disallow: /admin/
